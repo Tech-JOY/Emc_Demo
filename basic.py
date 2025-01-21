722cc670d39fb69f19a27ff8f7f01b416af22939
JOY D <joydaniel.jv@gmail.com>
2025-01-21 16:29:45 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,19 @@
+# Basics
+# 4/1/25
+
+'''a = input()
+b = input()
+print(a)
+print(b)
+print(type(a))
+print(type(b))'''
+'''
+# list with single parameters
+lis4 = ['This is an empty list']
+print(lis4)
+print(type(lis4))
+
+# tuple with no parameters
+tup1 = (1)
+print(tup1)
+print(type(tup1))'''
