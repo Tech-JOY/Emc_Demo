7cd4e39588bf708c2ab065c516adfda8fef1b1a2
JOY D <joydaniel.jv@gmail.com>
2025-01-21 16:34:53 +0530

    GSM_V2.py
    Here I activate the codes.

--- basic.py
+++ basic.py
@@ -1,13 +1,13 @@
 # Basics
 # 4/1/25
-# It is for practical Demo_1
-'''a = input()
+# It is for practical Demo_2
+a = input()
 b = input()
 print(a)
 print(b)
 print(type(a))
-print(type(b))'''
-'''
+print(type(b))
+
 # list with single parameters
 lis4 = ['This is an empty list']
 print(lis4)
@@ -16,4 +16,4 @@ print(type(lis4))
 # tuple with no parameters
 tup1 = (1)
 print(tup1)
-print(type(tup1))'''
+print(type(tup1))
